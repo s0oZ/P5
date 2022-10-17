61a808739f177cd0c60dfd4ccc5ce61676ba5967
Maxime Chivé <chive.max@gmail.com>
2022-10-17 19:46:25 +0200

    dernier probleme : alerte d ajout au panier

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,10 +1,8 @@
-let page = document.location.search;
+let page = document.location.search; //déclaration des variable
 let searchParams = new URLSearchParams(page);
 let idPage = searchParams.get("orderId");
 let orderID = document.getElementById("orderId");
 
 orderID.insertAdjacentHTML("beforeend", idPage);
-console.log(orderID);
-console.log(idPage);
 
-localStorage.clear();
+localStorage.removeItem("cart");
